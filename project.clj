5852206826eab98826cd05220def03cf0172781c
mwkuster <marc@budabe.eu>
2012-10-28 11:27:47 +0100

    Author information added

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject agents-for-actors "0.1.0-SNAPSHOT"
   :description "Agents for actors, a Digital Humanities framework for distributed, workflow-driven microservices for text analysis and visualization"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mwkuster/agents-for-actors"
   :license {:name "GNU Lesser Public License"
-            :url "http://www.gnu.org/copyleft/lesser.html"}
+            :url "http://www.gnu.org/copyleft/lesser.html"
+            :comments "Author: Marc Wilhelm Küster"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clojurewerkz/neocons "1.1.0-beta1"]]
   :main agents-for-actors.core
