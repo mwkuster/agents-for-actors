0326da0a2c1831490d2cc960c577a14dfd13e5d9
Marc <marc@budabe.eu>
2016-02-16 18:24:49 +0100

    Use of futures

--- src/agents_for_actors/core.clj
+++ src/agents_for_actors/core.clj
@@ -43,7 +43,7 @@
     (send xml-agent xml (x/parameters-tostr par/*parameters*))
     (let [link-futures
           (doall
-           (flatten ;Caution: map alone would build a list of lists
+           ;(flatten ;Caution: map alone would build a list of lists
               (map 
                #(future
                   (sim/find-similarities 
@@ -53,22 +53,18 @@
                    min-confidence
                    %
                    source-filtered))
-               target-filtered)))
-          links (map deref link-futures)]
+               target-filtered))
+          links (flatten (map deref link-futures))]
       (println "links")
-      (println links)
       (doseq
           [s links
            :when (not (empty? s)) ;explicitly exclude those runs that returned nil
            ]
-        (println "Individual hit")
-        (println s)))
-       ;; (do
-       ;;   (send-off visualization-agent vis/visualize 
-       ;;         (x/xpointer-tostr (:phrase s))
-       ;;         (:source s) :cites)
-       ;;   (send xml-agent xml (str "<result confidence='" (:confidence s) "'><phrase hit='" (:t1 s) "'>" (x/loc-tostr (:phrase s)) "</phrase><source hit='" (:t2 s) "'>" (x/loc-tostr (:source s)) "</source></result>"))
-       ;; )))
+        (send-off visualization-agent vis/visualize 
+                  (x/xpointer-tostr (:phrase s))
+                  (:source s) :cites)
+        (send xml-agent xml (str "<result confidence='" (:confidence s) "'><phrase hit='" (:t1 s) "'>" (x/loc-tostr (:phrase s)) "</phrase><source hit='" (:t2 s) "'>" (x/loc-tostr (:source s)) "</source></result>"))
+       ))
     (send xml-agent xml "</results>")
     (send-off visualization-agent vis/finalize source-file)
     (println "Preparing for shutdown")
