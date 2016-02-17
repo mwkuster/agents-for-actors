4110698ce0f7866bd6ea262b62db000462869b2c
Marc <marc@budabe.eu>
2016-02-17 08:48:24 +0100

    send instead of send-off for visualization agent

--- src/agents_for_actors/similitude.clj
+++ src/agents_for_actors/similitude.clj
@@ -8,7 +8,7 @@ a sequence of locations) and return a sequence of maps of source and
 phrase locations pointing to those similarities, including the
 respective confidence. Use a given function (e.g. quadgram coupled
 with a dice coefficient) that returns a confidence in [0..1] for
-comparision"
+comparision. The provided similarity function must return a map with at least the keys :weight, :phrase, :source, :confidence, :t1 (for the best hit in the first phrase) and :t2 (for the best hit in the second phrase). the function returns a list of non-empty such maps with a weight above min-confidence"
   (let
       [res (for [p-loc source-seq 
                  :let [best-hit (similarity-fn (z/node p-loc) (z/node phrase-loc))
