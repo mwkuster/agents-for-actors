f15663cdfa311df5ed5a05b0bf0b9b398626cdd4
Marc <marc@budabe.eu>
2016-02-17 14:58:54 +0100

    Partitioning improved

--- src/agents_for_actors/similitude.clj
+++ src/agents_for_actors/similitude.clj
@@ -3,7 +3,7 @@
             [clojure.zip :as z]))
 
 (defn- part-after-phrase-loc [phrase-loc source-seq]
-  "If input and output files are identical, the phrase will be part of the source sequence. To avoid parsing the same lines twice, ignore all lines before the current phrase (optimization)"
+  "If input and output files are identical, the phrase will be part of the source sequence. To avoid parsing the same lines twice, ignore all lines before the current phrase (optimization). If the last line has been reached, return nil"
   (let
       [remainder (partition-by #(= % phrase-loc) source-seq)]
     (if (= (count remainder) 3)
