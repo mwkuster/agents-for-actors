b7e7025925e2692886c5bf668c9b7060d1a81db6
Marc <marc@budabe.eu>
2016-02-17 13:15:32 +0100

    Optimization for parsing twice the same file, avoiding double comparisons

--- src/agents_for_actors/similitude.clj
+++ src/agents_for_actors/similitude.clj
@@ -2,7 +2,16 @@
   (:require [clojure.string :as cs]
             [clojure.zip :as z]))
 
-(defn find-similarities [similarity-fn ^Double min-confidence phrase-loc source-seq]
+(defn- part-after-phrase-loc [phrase-loc source-seq]
+  "If input and output files are identical, the phrase will be part of the source sequence. To avoid parsing the same lines twice, ignore all lines before the current phrase (optimization)"
+  (println "Partioning")
+  (last (partition-by #(= % phrase-loc) source-seq)))
+
+
+(defn find-similarities
+  ([similarity-fn ^Double min-confidence phrase-loc source-seq]
+   (find-similarities similarity-fn min-confidence phrase-loc source-seq false))
+  ([similarity-fn ^Double min-confidence phrase-loc source-seq input-equals-output?] 
   "Find similarities for a given phrase (= a location) in a source (=
 a sequence of locations) and return a sequence of maps of source and
 phrase locations pointing to those similarities, including the
@@ -10,12 +19,15 @@ respective confidence. Use a given function (e.g. quadgram coupled
 with a dice coefficient) that returns a confidence in [0..1] for
 comparision. The provided similarity function must return a map with at least the keys :weight, :phrase, :source, :confidence, :t1 (for the best hit in the first phrase) and :t2 (for the best hit in the second phrase). the function returns a list of non-empty such maps with a weight above min-confidence"
   (let
-      [res (for [p-loc source-seq 
+      [source-seq-reduced
+       (if input-equals-output?
+         (part-after-phrase-loc phrase-loc source-seq)
+         source-seq)
+       res (for [p-loc source-seq-reduced 
                  :let [best-hit (similarity-fn (z/node p-loc) (z/node phrase-loc))
                        confidence (:weight best-hit)]
                  :when (> confidence min-confidence)]
              (do
                {:phrase phrase-loc :source p-loc :confidence confidence :t1 (:t1 best-hit) :t2 (:t2 best-hit)}))]
-       (filter #(not (empty? %)) res)))
-      
+       (filter #(not (empty? %)) res))))
         
